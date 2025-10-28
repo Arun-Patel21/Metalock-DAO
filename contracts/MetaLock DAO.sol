4d93319efc4da081a703a4bb77f64417dd3c72fd
Arun-Patel21 <arungurjar9301@gmail.com>
2025-10-28 20:16:31 +0530

    Contract updated

--- contracts/MetaLock DAO.sol
+++ contracts/MetaLock DAO.sol
@@ -1,52 +1,9 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract MetaLockDAO {
-    address public owner;
-    uint256 public proposalCount;
-
-    struct Proposal {
-        uint256 id;
-        string description;
-        uint256 votes;
-        bool executed;
-    }
-
-    mapping(uint256 => Proposal) public proposals;
-    mapping(address => bool) public members;
-
-    event ProposalCreated(uint256 id, string description);
-    event Voted(uint256 proposalId, address voter);
-    event Executed(uint256 proposalId);
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not authorized");
-        _;
-    }
-
-    modifier onlyMember() {
-        require(members[msg.sender], "Not a DAO member");
-        _;
-    }
-
-    constructor() {
-        owner = msg.sender;
-        members[msg.sender] = true;
-    }
-
-    // Function 1: Add new DAO member
+Function 1: Add new DAO member
     function addMember(address _member) public onlyOwner {
         members[_member] = true;
     }
 
-    // Function 2: Create new proposal
-    function createProposal(string memory _description) public onlyMember {
-        proposalCount++;
-        proposals[proposalCount] = Proposal(proposalCount, _description, 0, false);
-        emit ProposalCreated(proposalCount, _description);
-    }
-
-    // Function 3: Vote on a proposal
+    Function 3: Vote on a proposal
     function vote(uint256 _proposalId) public onlyMember {
         require(_proposalId <= proposalCount && _proposalId > 0, "Invalid proposal");
         proposals[_proposalId].votes++;
@@ -61,3 +18,6 @@ contract MetaLockDAO {
         emit Executed(_proposalId);
     }
 }
+// 
+update
+// 
